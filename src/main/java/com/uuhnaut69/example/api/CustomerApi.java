b2ad6fad054bf2baa896eed8880c4122aae06bec
uuhnaut69 <uuhnaut96@gmail.com>
2021-06-18 09:34:54 +0700

    Add request validation

--- src/main/java/com/uuhnaut69/example/api/CustomerApi.java
+++ src/main/java/com/uuhnaut69/example/api/CustomerApi.java
@@ -1,13 +1,14 @@
 package com.uuhnaut69.example.api;
 
+import com.uuhnaut69.example.domain.customer.CustomerDTO;
 import com.uuhnaut69.example.domain.customer.entity.Customer;
 import com.uuhnaut69.example.domain.customer.ports.CustomerService;
-import com.uuhnaut69.example.domain.customer.CustomerDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
@@ -19,13 +20,13 @@ public class CustomerApi {
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public Mono<Customer> createCustomer(final @RequestBody CustomerDTO customerDTO) {
+  public Mono<Customer> createCustomer(final @RequestBody @Valid CustomerDTO customerDTO) {
     return customerService.createCustomer(customerDTO);
   }
 
   @PutMapping("/{customerId}")
   public Mono<Customer> updateCustomer(
-      final @PathVariable UUID customerId, final @RequestBody CustomerDTO customerDTO) {
+      final @PathVariable UUID customerId, final @RequestBody @Valid CustomerDTO customerDTO) {
     return customerService.updateCustomer(customerId, customerDTO);
   }
 
--- src/main/java/com/uuhnaut69/example/domain/customer/CustomerDTO.java
+++ src/main/java/com/uuhnaut69/example/domain/customer/CustomerDTO.java
@@ -4,14 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerDTO {
 
-  private String username;
+  @NotBlank private String username;
 
-  private String firstName;
+  @NotBlank private String firstName;
 
-  private String lastName;
+  @NotBlank private String lastName;
 }
